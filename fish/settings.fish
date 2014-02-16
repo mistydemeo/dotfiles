6e9cd0ade95917ff03a8de39cb24af88f3c03b7f
Misty De Meo <mistydemeo@gmail.com>
2014-02-15 21:15:36 -0800

    Set up PATH before calling any commands

--- fish/settings.fish
+++ fish/settings.fish
@@ -1,11 +1,14 @@
-fortune
-
+# PATH needs to be set up before any paths are called,
+# because Tiger doesn't include /usr/local/bin in the
+# default PATH.
 for p in /usr/local/opt/ruby/bin /usr/local/sbin /usr/local/bin
 	set PATH $p $PATH
 end
 
 # PATH should take priority over any duplicates that live here
 set PATH $PATH /usr/local/texlive/2011/bin/x86_64-darwin
+
+fortune
 
 if test -n "$SSH_CLIENT"
 	set -x EDITOR "rsub"
