60579263ba560514c3e25d9a52209d860cfbcc28
Misty De Meo <mdemeo@artefactual.com>
2014-11-17 15:56:26 -0800

    Set up Archivematica PYTHONPATH

--- fish/Linux.fish
+++ fish/Linux.fish
@@ -2,3 +2,7 @@
 source /usr/local/share/chruby/chruby.fish
 chruby 2.0.0 >/dev/null
 set -x TERM xterm-256color
+
+set -x DJANGO_SETTINGS_MODULE settings.local
+set -x PYTHONPATH /usr/share/archivematica/dashboard $PYTHONPATH
+set -x PYTHONPATH /usr/lib/archivematica/archivematicaCommon $PYTHONPATH
