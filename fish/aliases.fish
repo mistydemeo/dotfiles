14b47c03a529a59ae26f7bb0a43f0604df7502fa
Misty De Meo <mistydemeo@gmail.com>
2015-07-31 17:16:07 -0700

    mb completion: don't use __fish_git_branches

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,4 +1,4 @@
 function mb
   git merge $argv[1]; and git push; and git branch -d $argv[1]; and git push origin --delete $argv[1]
 end
-complete -f -c mb -a '(__fish_git_branches)' -d 'Merge and delete branch'
+complete -f -c mb -a "(git branch --no-color -a ^/dev/null | sgrep -v ' -> ' | sed -e 's/^..//' -e 's/^remotes\///')" -d 'Merge and delete branch'
