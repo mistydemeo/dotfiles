ae75e3429d4431c5a5ded791e95a54a10650839d
Misty De Meo <mistydemeo@gmail.com>
2016-02-21 00:31:42 -0800

    Add disable_line_ducking to Darwin

--- fish/Darwin.fish
+++ fish/Darwin.fish
@@ -1,1 +1,5 @@
 alias tatiana="ssh -A -L 8000:localhost:8000 -R 52698:localhost:52698 mdemeo@tatiana.local"
+
+# Disables OS X's audio ducking, which normally reduces system volume during a call.
+# FaceTime uses the same function but it can't be changed due to SIP.
+alias disable_line_ducking="printf \"p *(char*)(void(*)())AudioDeviceDuck=0xc3\nq\" | lldb -n LINE"
