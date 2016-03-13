404fae4b14f80f1ce08527cced9301c45ada5a6f
Misty De Meo <mistydemeo@gmail.com>
2016-03-13 11:09:33 -0700

    Remove Artefactual-specific Linux.fish

--- fish/Linux.fish
+++
@@ -1,8 +0,0 @@
-# Enable chruby and switch to default ruby
-source /usr/local/share/chruby/chruby.fish
-test (which chruby); and chruby 2.2.3 >/dev/null
-set -x TERM xterm-256color
-
-set -x DJANGO_SETTINGS_MODULE settings.local
-set -x PYTHONPATH /usr/share/archivematica/dashboard $PYTHONPATH
-set -x PYTHONPATH /usr/lib/archivematica/archivematicaCommon $PYTHONPATH
