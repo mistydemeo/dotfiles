03938a121bf3ee070aeb13f9a6744eb2fbea9c92
Misty De Meo <mistydemeo@gmail.com>
2020-02-15 15:39:45 -0800

    fish: fix rbenv/nodenv setup

--- fish/config.fish
+++ fish/config.fish
@@ -39,8 +39,8 @@ source ~/.config/fish/prompt.fish
 test -e ~/.iterm2_shell_integration.fish; and source ~/.iterm2_shell_integration.fish
 
 # Set up nodenv and rbenv, but only if installed
-status --is-interactive; and type --quiet nodenv; and . (nodenv init -|psub)
-status --is-interactive; and type --quiet rbenv; and . (rbenv init -|psub)
+status --is-interactive; and type --quiet nodenv; and source (nodenv init -|psub)
+status --is-interactive; and type --quiet rbenv; and source (rbenv init -|psub)
 
 # Make sure GPG knows the right terminal to use
 set -x GPG_TTY (tty)
